027dd1be4264c292e9afaf786dcc0a4c57b4e12c
riyajj2 <riyajj2@fa25-cs598a-031.cs.illinois.edu>
2025-11-06 15:34:43 -0600

    metropolis opt

--- main.c
+++ main.c
@@ -68,15 +68,42 @@ double calculateMagnetization() {
   return mag / (L * L);
 }
 
+// void metropolisHastingsStep() {
+//   int i = (int)(randomDouble() * L);
+//   int j = (int)(randomDouble() * L);
+
+//   double E_before = calculateTotalEnergy();
+//   lattice[i][j] *= -1;
+//   double E_after = calculateTotalEnergy();
+//   double dE = E_after - E_before;
+
+//   if (dE <= 0.0) {
+//     return;
+//   }
+
+//   double prob = exp(-dE / T);
+//   if (randomDouble() >= prob) {
+//     lattice[i][j] *= -1;
+//   }
+// }
+
 void metropolisHastingsStep() {
   int i = (int)(randomDouble() * L);
   int j = (int)(randomDouble() * L);
 
-  double E_before = calculateTotalEnergy();
+  int spin = lattice[i][j];
+  
+  int up = lattice[(i - 1 + L) % L][j];
+  int down = lattice[(i + 1) % L][j];
+  int left = lattice[i][(j - 1 + L) % L];
+  int right = lattice[i][(j + 1) % L];
+  
   lattice[i][j] *= -1;
-  double E_after = calculateTotalEnergy();
-  double dE = E_after - E_before;
 
+  double E_before = -J * spin * (up + down + left + right);
+  double E_after = -J * (-spin) * (up + down + left + right);
+  double dE = E_after - E_before;
+  
   if (dE <= 0.0) {
     return;
   }
